9e8e91acc5ffb279a1bb484771733cb63c82f450
akhil <akhil@zdistancelab.com>
2025-12-17 11:44:18 +0530

    updated plugin to proper Python packages

--- tutormyplugin/tutormyplugin/plugin.py
+++ tutormyplugin/tutormyplugin/plugin.py
@@ -9,22 +9,13 @@ __version__ = "20.0.0"
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "openedx-dockerfile-post-python-requirements",
-        """
-RUN pip install git+https://github.com/akhilmohankdr/tutormyplugin.git@main
-"""
+        "RUN pip install git+https://github.com/akhilmohankdr/tutormyplugin.git@main"
     )
 )
-
-##########################################################
-# Add to INSTALLED_APPS
-##########################################################
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "openedx-lms-common-settings",
-        """
-INSTALLED_APPS.append('tutormyplugin.my_api')
-ALLOWED_HOSTS.append('apps.local.openedx.io')
-"""
+        "INSTALLED_APPS.append('tutormyplugin.my_api')"
     )
 )
 
@@ -37,4 +28,4 @@ def urls():
         path("api/myplugin/", include("tutormyplugin.my_api.urls")),
     ]
 
-hooks.Filters.LMS_URLS.add_item(urls)
+hooks.Filters.ENV_PATCHES.add_item(urls)
