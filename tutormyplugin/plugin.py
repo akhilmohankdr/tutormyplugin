afec9a53e2a3984c340af79c6019496d2c803232
akhil <akhil@zdistancelab.com>
2025-12-17 13:53:20 +0530

    updated the plugin to avoid launch

--- tutormyplugin/plugin.py
+++ tutormyplugin/plugin.py
@@ -2,7 +2,6 @@ from tutor import hooks
 import os
 
 __version__ = "20.0.0"
-
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 config = {
@@ -11,7 +10,9 @@ config = {
     }
 }
 
+##########################################################
 # Install plugin via pip during image build
+##########################################################
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "openedx-dockerfile-post-python-requirements",
@@ -19,7 +20,43 @@ hooks.Filters.ENV_PATCHES.add_item(
     )
 )
 
+##########################################################
+# Patch urls.py during build
+##########################################################
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "openedx-lms-production-settings",
+        """
+# Myplugin URL configuration
+from django.urls import path, include
+import sys
+
+# Add myplugin URLs to urlpatterns
+if 'tutormyplugin.my_api.urls' not in str(ROOT_URLCONF):
+    # Import at runtime to avoid circular imports
+    def _add_myplugin_urls():
+        from django.conf.urls import url
+        from django.urls import path, include
+        try:
+            import tutormyplugin.my_api.urls
+            return [path('api/myplugin/', include('tutormyplugin.my_api.urls'))]
+        except ImportError:
+            return []
+    
+    # This will be executed when Django loads
+    from django.conf import settings
+    if hasattr(settings, 'ROOT_URLCONF'):
+        import importlib
+        urlconf = importlib.import_module(settings.ROOT_URLCONF)
+        if hasattr(urlconf, 'urlpatterns'):
+            urlconf.urlpatterns.extend(_add_myplugin_urls())
+"""
+    )
+)
+
+##########################################################
 # Add to INSTALLED_APPS
+##########################################################
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "openedx-lms-common-settings",
@@ -29,13 +66,4 @@ INSTALLED_APPS.append('tutormyplugin.my_api')
 ALLOWED_HOSTS.append('apps.local.openedx.io')
 """
     )
-)
-
-# Register init task
-hooks.Filters.CLI_DO_INIT_TASKS.add_item(
-    ("lms", ("myplugin", "init"))
-)
-# Add URL configuration via template
-hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
-    os.path.join(HERE, "templates")
 )
